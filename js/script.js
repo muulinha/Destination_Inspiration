da83a111604aa620b80638b05f35f8b838b59e68
Aaron Raikes <98355587+aaronraikes@users.noreply.github.com>
2022-12-22 14:23:58 -0800

    added local storage and history

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function fetchWeather(city) {
 
 
 
- 
+
   fetchWeather();
 // COUNTRY CODE OUTPUT
 
@@ -47,3 +47,67 @@ fetch('https://countries-cities.p.rapidapi.com/location/country/GB', options)
     // CURRENCY CODE OUTPUT
 
     // https://countries-cities.p.rapidapi.com/location/country/GB/576a507ee7msh03a8d40416350bbp1e0201jsnbd061cc4d364
+
+
+
+
+    // LOCAL STORAGE
+
+    var searchInput = document.querySelector("#search-text");  
+    
+    var ourSearchHistory = JSON.parse(localStorage.getItem('seachHistory'));
+
+    // get input value
+    function storeSearch() {
+      var new_data = document.getElementById('search-text').value;
+
+      // does array aleady exist
+      if(localStorage.getItem('searchHistory') == null) {
+        localStorage.setItem('searchHistory', '[]')
+      }
+
+      // check if city already exists in local storage
+      var old_data = JSON.parse(localStorage.getItem('searchHistory'));
+      console.log(old_data.includes(new_data));
+      if(old_data.includes(new_data)) {
+        return;
+      }
+      old_data.push(new_data);
+      localStorage.setItem('searchHistory', JSON.stringify(old_data));
+    }
+    
+    var searchForm = document.querySelector("#search-submit");
+    
+    // trigger search if value is not blank
+    searchForm.addEventListener("click", function(event) {
+      event.preventDefault();
+      var searchText = searchInput.value.trim();
+      if (searchText === "") {
+        return;
+      }
+      storeSearch();
+    });
+
+    // END LOCAL STORAGE
+
+
+
+    // HISTORY
+
+      // get previous searches and create dropdown
+      $(function () {
+        var dropdownHistory = JSON.parse(localStorage.getItem('searchHistory'));
+        $('#search-text').autocomplete({
+          source: dropdownHistory,
+          minLength: 0,
+        });
+      });
+
+      var delForm = document.querySelector("#delSearchHistory");
+      
+      // delete search history data only
+      delForm.addEventListener("click", function(event) {
+        localStorage.removeItem('searchHistory');
+      });     
+
+    // END HISTORY
